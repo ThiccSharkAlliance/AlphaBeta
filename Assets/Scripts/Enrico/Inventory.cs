fb16cb08da2492bfee233df744867a02248642cd
Enrico <sandri.enrico86@gmail.com>
2021-03-02 14:03:25 +0000

    local .json change

--- Assets/Scripts/Enrico/Inventory.cs
+++ Assets/Scripts/Enrico/Inventory.cs
@@ -24,16 +24,16 @@ public class Inventory : MonoBehaviour
         PlayFabClientAPI.GetCatalogItems(catalogItems, result =>
         {
             List<CatalogItem> items = result.Catalog;
-            
+           // result.CustomData //////////////////////////////////TO TEST WITH THIS and with more keys per item
             foreach (CatalogItem i in items)
             {
                 uint cost = i.VirtualCurrencyPrices["SL"];
                 
                 ///////JSON STUFF HERE/////////////////////////////
-                string json = i.CustomData; // Store cloudData into a string
-                File.WriteAllText(Application.dataPath + "/savedJson.json", json); //Write a json file to assets
+                string json = i.CustomData;                                                 // Store cloudData into a string
+                File.WriteAllText(Application.dataPath + "/savedJson.json", json);         //Write a json file to assets
                 string read = File.ReadAllText(Application.dataPath + "/savedJson.json"); //Read from json file and store it
-                JsonDataHolder jsonSet = JsonUtility.FromJson<JsonDataHolder>(read);// Apply the read data and set the data the json class.
+                JsonDataHolder jsonSet = JsonUtility.FromJson<JsonDataHolder>(read);     // Apply the read data and set the data the json class.
                 ////////////////////////////////////////////////
                 
                 foreach (var info in manager.resourceInfo)
