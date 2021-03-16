5f3c0581b053343e31ceea42b7830303762ac004
dave <dnewman.1912@gmail.com>
2021-03-16 18:18:41 +0000

    VFX bug fixes and set up box destruction effect
    can now switch between sphere and box destruction methods for terrain and vfx will run two different effects

--- Assets/Scripts/VoxelTerrain/Interactions/VfxInteraction.cs
+++ Assets/Scripts/VoxelTerrain/Interactions/VfxInteraction.cs
@@ -13,6 +13,9 @@ public struct VfxInteraction
     [SerializeField] private string _spawnRateStringId;
     [SerializeField] private int _spawnRate;
 
+    [SerializeField] private string _spawnRateBoxStringId;
+    [SerializeField] private int _spawnRateBox;
+
     [SerializeField] private string _spawnPointStringId;
 
     [SerializeField] private string _particleLifeStringId;
@@ -21,8 +24,10 @@ public struct VfxInteraction
     [SerializeField] private string _sphereRadiusStringId;
     [SerializeField] private float _sphereRadius;
 
-    [SerializeField] private string _boxRadiusStringId;
-    [SerializeField] private float _boxRadius;
+    [SerializeField] private string _boxRadiusXStringId;
+    [SerializeField] private float _boxRadiusX;
+    [SerializeField] private string _boxRadiusZStringId;
+    [SerializeField] private float _boxRadiusZ;
 
     [SerializeField] private string _planeRadiusStringId;
     [SerializeField] private float _planeRadius;
@@ -32,6 +37,9 @@ public struct VfxInteraction
     
     [SerializeField] private string _sparkSpawnRateStringId;
     [SerializeField] private int _sparkSpawnRate;
+
+    
+    
 
     public void VfxPlaya(Vector3 spawnPoint, FlattenShape shape, InteractionSettings _interactionSettings)
     {
@@ -44,7 +52,13 @@ public struct VfxInteraction
 
                 break;
             case FlattenShape.Square:
-           
+                _vfx.SetInt(_spawnRateBoxStringId, _spawnRateBox);
+                _vfx.SetFloat(_particleLifeStringId, _particleLife);
+                _vfx.SetFloat(_boxRadiusXStringId, _boxRadiusX = _interactionSettings.CubeXDistance);
+                _vfx.SetFloat(_boxRadiusZStringId, _boxRadiusZ = _interactionSettings.CubeZDistance);
+                _vfx.SetVector3(_spawnPointStringId, spawnPoint); 
+                _vfx.SetInt(_sparkSpawnRateStringId, _sparkSpawnRate);
+                _vfx.SetInt(_spawnRateStringId, 0);
 
 
                 break;
@@ -58,8 +72,9 @@ public struct VfxInteraction
                 _vfx.SetFloat(_particleLifeStringId, _particleLife);
                 _vfx.SetFloat(_sphereRadiusStringId, _sphereRadius = _interactionSettings.SphereRadius / 100f + 0.04f);
                 _vfx.SetVector3(_spawnPointStringId, spawnPoint); 
-                _vfx.SetFloat(_particleRingCountStringId, _spawnRate); // this is not an accidient, ring count needs to be the same as the spawn rate or higher
-                _vfx.SetInt(_sparkSpawnRateStringId, _sparkSpawnRate); 
+                _vfx.SetFloat(_particleRingCountStringId, _particleRingCount); // this is not an accidient, ring count needs to be the same as the spawn rate or higher
+                _vfx.SetInt(_sparkSpawnRateStringId, _sparkSpawnRate);
+                _vfx.SetInt(_spawnRateBoxStringId, 0);
 
                 break;
             default:
@@ -67,9 +82,10 @@ public struct VfxInteraction
         }
     }
 
-    public void VfxStopa()
+    public void VfxStopa() // may need to add a small delay before this method is run on the voxelinteraction script
     {
         _vfx.SetInt(_spawnRateStringId, 0);
         _vfx.SetInt(_sparkSpawnRateStringId, 0);
+        _vfx.SetInt(_spawnRateBoxStringId, 0);
     }
 }
