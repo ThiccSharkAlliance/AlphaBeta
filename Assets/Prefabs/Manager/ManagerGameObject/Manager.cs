b12748c6d1507c7ae63a6bae49a80f4af789628b
Ian <barrowian1991@gmail.com>
2021-04-13 15:59:38 +0100

    Updated models and assets for scene

--- Assets/Prefabs/Manager/ManagerGameObject/Manager.cs
+++ Assets/Prefabs/Manager/ManagerGameObject/Manager.cs
@@ -103,7 +103,7 @@ public class Manager : MonoBehaviour
     IEnumerator StartAfterPause(Vector3 startPoint, GameObject playerShip)
     {
         yield return new WaitForSeconds(initialisationPause);
-        GameObject player = GameObject.Instantiate(playerShip, startPoint + new Vector3(0,1,0), Quaternion.identity);
+        GameObject player = GameObject.Instantiate(playerShip, startPoint + new Vector3(10,1,10), Quaternion.identity);
     }
 
     public void RebuildSelectableBuildings()
