47206688ea79c55f82c3d56bdaf00663435d4763
Ian <barrowian1991@gmail.com>
2021-05-03 12:23:56 +0100

    Added tree voxel types to ban list

--- Assets/Scripts/VoxelTerrain/Interactions/InteractionBan.cs
+++ Assets/Scripts/VoxelTerrain/Interactions/InteractionBan.cs
@@ -4,6 +4,17 @@ namespace VoxelTerrain.Interactions
 {
     public static class InteractionBan
     {
-        public static readonly VoxelType[] BanList = {VoxelType.Water};
+        public static readonly VoxelType[] BanList = {
+            VoxelType.Water,         
+            VoxelType.OakBranch,
+            VoxelType.OakLeaf,
+            VoxelType.JungleBranch,
+            VoxelType.JungleLeaf,
+            VoxelType.SavannahBranch,
+            VoxelType.SavannahLeaf,
+            VoxelType.PineBranch,
+            VoxelType.PineLeaf,
+            VoxelType.SwampBranch,
+            VoxelType.SwampLeaf};
     }
 }
