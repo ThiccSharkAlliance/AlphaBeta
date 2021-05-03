6b803cda987aa5c08a8e9db2cccddd844a222ccb
dave <dnewman.1912@gmail.com>
2021-05-03 13:20:11 +0100

    Updated new branch with vfx files from a separate unity project

---
+++ Assets/VFX Scripts/SharkWeapons.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class SharkWeapons : MonoBehaviour
+{
+    VisualEffect sharkdoken;
+    public float timer;
+    public bool charged;
+    public float charging;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        sharkdoken = GetComponent<VisualEffect>();
+        sharkdoken.SetFloat("fireprojectile", 0);
+        sharkdoken.SetFloat("chargerate", 0);
+        //sharkdoken.initialEventName = "Charge";
+
+
+        timer = 0f;
+        charged = false;
+        charging = 0f;
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            sharkdoken.SetFloat("chargerate", 11);
+           
+            
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            timer = 0f;
+           
+        }
+        if (charged == true)
+        {
+            timer = 0f;
+            sharkdoken.SetFloat("fireprojectile", 1);
+
+        }
+    }
+}
